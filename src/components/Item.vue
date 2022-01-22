a67c7c28cf394ba349da3d46e479e038af51bd6d
thehouseplant <sean.collins@outlook.com>
2022-01-22 12:28:03 -0800

    Add item components

---
+++ src/components/Item.vue
@@ -0,0 +1,71 @@
+<template>
+  <span>
+    <span>
+      <span>
+        <a class="underline" :href="url" target="_blank">
+          {{ title }}
+        </a>
+      </span>
+      <span v-if="domain">
+        ({{ domain }})
+      </span>
+    </span>
+    <p>
+      <small>
+        <span v-if="points">
+          {{ points }} points
+        </span>
+        <span v-if="user">
+          by <router-link class="underline" :to="`/user/${user}`">{{ user }}</router-link> |
+        </span>
+        <span>
+          Posted {{ timeAgo }}
+        </span>
+        <span v-if="commentsCount > 0">
+          | <router-link class="underline" :to="`/item/${id}`">{{ commentsCount }} comments</router-link>
+        </span>
+      </small>
+    </p>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Item',
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    points: {
+      type: Number,
+      required: true
+    },
+    user: {
+      type: String,
+      required: true
+    },
+    timeAgo: {
+      type: String,
+      required: true
+    },
+    commentsCount: {
+      type: Number,
+      required: false,
+      default: 0
+    },
+    domain: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,9 +1,16 @@
 <template>
   <h1>Top</h1>
+
+  <ItemList />
 </template>
 
 <script>
+import ItemList from '@/components/ItemList.vue'
+
 export default {
-  name: 'Top'
+  name: 'Top',
+  components: {
+    ItemList
+  }
 }
 </script>
